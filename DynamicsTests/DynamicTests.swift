38c7b3811b0e6f55f03c69dcce1374ed700b3abc
jsbean <jbean46@gmail.com>
2016-04-27 22:25:37 -0400

    Add initial tests

--- DynamicsTests/DynamicTests.swift
+++ DynamicsTests/DynamicTests.swift
@@ -7,8 +7,59 @@
 //
 
 import XCTest
+@testable import Dynamics
 
 class DynamicTests: XCTestCase {
 
-
+    // MARK: - Init with Elements
+    
+    func testInitWithEmptyElements() {
+        XCTAssertNil(Dynamic(elements: []))
+    }
+    
+    func testInitWithElementsPiano() {
+        XCTAssertNotNil(Dynamic(elements: [.Piano]))
+    }
+    
+    func testInitWithElementsForte() {
+        XCTAssertNotNil(Dynamic(elements: [.Forte]))
+    }
+    
+    func testInitWithElementsNiente() {
+        XCTAssertNotNil(Dynamic(elements: [.Niente]))
+    }
+    
+    func testInitWithElementsMezzoPiano() {
+        XCTAssertNotNil(Dynamic(elements: [.Mezzo, .Piano]))
+    }
+    
+    func testInitWithElementsMezzoForte() {
+        XCTAssertNotNil(Dynamic(elements: [.Mezzo, .Forte]))
+    }
+    
+    func testInitWithElementsFortissimo() {
+        XCTAssertNotNil(Dynamic(elements: [.Forte, .Forte]))
+    }
+    
+    // MARK: - Init with String
+    
+    func testInitWithEmptyThrows() {
+        do {
+            let _ = try Dynamic(string: "")
+            XCTFail()
+        } catch { }
+    }
+    
+    func testInitWithStringP() {
+        do { let _ = try Dynamic(string: "p") } catch { XCTFail() }
+    }
+    
+    func testInitWithStringF() {
+        do { let _ = try Dynamic(string: "f") } catch { XCTFail() }
+    }
+    
+    func testInitWithStringO() {
+        do { let _ = try Dynamic(string: "o") } catch { XCTFail() }
+    }
 }
+
