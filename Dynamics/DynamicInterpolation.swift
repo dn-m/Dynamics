8e1bce1ebdf55252ae7bc26951616b765872baa5
jsbean <jbean46@gmail.com>
2016-05-01 15:45:13 -0400

    Refine doc comments

--- Dynamics/DynamicInterpolation.swift
+++ Dynamics/DynamicInterpolation.swift
@@ -8,20 +8,44 @@
 
 import Foundation
 
+/**
+ Structure defining the interpolation between to `Dynamic` objects. Rendered tradiationally as
+ a "hairpin".
+ */
 public struct DynamicInterpolation {
     
+    /**
+     Direction of interpolation.
+     */
     public enum Direction {
-        case Crescendo, Decrescendo, Static
+        
+        case Crescendo
+        
+        case Decrescendo
+        
+        case Static
         
         init(_ start: Dynamic, _ stop: Dynamic) {
             self = start == stop ? .Static : start < stop ? .Crescendo : .Decrescendo
         }
     }
     
+    // MARK: - Instance properties
+    
+    /// `Dynamic` object at start of interpolation.
     public let startDynamic: Dynamic
+    
+    /// `Dynamic` object at stop of interpolation.
     public let stopDynamic: Dynamic
+    
+    /// `Direction` of interpolation.
     public let direction: Direction
     
+    // MARK: - Initializers
+    
+    /**
+     Create a `DynamicInterpolation` with `start` and `stop` `Dynamic` values.
+     */
     public init(from start: Dynamic, to stop: Dynamic) {
         self.startDynamic = start
         self.stopDynamic = stop
